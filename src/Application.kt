1abd49854bd0aea3956a8bff52c854947e667fff
Manuel Lorenzo <manulorenzop@gmail.com>
2019-06-30 18:55:40 +0200

    adds hello routing

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,22 @@
 package me.manulorenzo
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import me.manulorenzo.webapp.home
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText { "Hello world!" }
+        }
+        home()
+    }
 }
 
