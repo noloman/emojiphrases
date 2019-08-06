0b4f3b0c2b1426dc562a3c2efa0a6685156ca3a3
Manuel Lorenzo <manulorenzop@gmail.com>
2019-08-06 08:48:14 +0200

    changes Phrase.kt to use Ktor locations :zap:

--- src/webapp/api/Phrase.kt
+++ src/webapp/api/Phrase.kt
@@ -1,18 +1,25 @@
 package me.manulorenzo.webapp.api
 
 import io.ktor.application.call
+import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.locations.Location
+import io.ktor.locations.post
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
-import io.ktor.routing.post
 import me.manulorenzo.API_VERSION
 import me.manulorenzo.webapp.model.Request
 import me.manulorenzo.webapp.repository.Repository
 
 const val PHRASE_ENDPOINT = "$API_VERSION/phrase"
 
+@KtorExperimentalLocationsAPI
+@Location(PHRASE_ENDPOINT)
+class Phrase
+
+@KtorExperimentalLocationsAPI
 fun Route.phrase(db: Repository) {
-    post(PHRASE_ENDPOINT) {
+    post<Phrase> {
         val request = call.receive<Request>()
         val phrase = db.add("", request.emoji, request.phrase)
         call.respond(phrase)
